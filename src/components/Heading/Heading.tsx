619242e6a16f9383208a7983656f928c79df4076
Farid Hosman <farid.hosman@adidas.com>
2024-09-30 09:56:30 -0500

    Adds font size units to Heading component

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -7,32 +7,39 @@ interface HeadingProps {
   fontSize?: number
   color?: "pink" | "yellow" | "purple" | "fuchsia"
   wrapText?: boolean
+  units: "px" | "%" | "em" | "rem"  
 }
-export const Heading = ({children, fontSize = 220, color = "fuchsia", wrapText = true}:HeadingProps)=> {
+export const Heading = ({
+  children,
+  fontSize = 220,
+  color = "fuchsia",
+  wrapText = true,
+  units = "px"
+}:HeadingProps)=> {
   return(
     <HeadingContainer>
-      <HeadingShadowsStyle fontSize={fontSize} color={color} $wrapText={wrapText}>
+      <HeadingShadowsStyle fontSize={fontSize} color={color} $wrapText={wrapText} units={units}>
         {children}
       </HeadingShadowsStyle>
-      <HeadingShadowsStyle fontSize={fontSize} color={color} $wrapText={wrapText}>
+      <HeadingShadowsStyle fontSize={fontSize} color={color} $wrapText={wrapText} units={units}>
         {children}
       </HeadingShadowsStyle>
-      <HeadingShadowsStyle fontSize={fontSize} color={color} $wrapText={wrapText}>
+      <HeadingShadowsStyle fontSize={fontSize} color={color} $wrapText={wrapText} units={units}>
         {children}
       </HeadingShadowsStyle>
-      <HeadingShadowsStyle fontSize={fontSize} color={color} $wrapText={wrapText}>
+      <HeadingShadowsStyle fontSize={fontSize} color={color} $wrapText={wrapText} units={units}>
         {children}
       </HeadingShadowsStyle>
-      <HeadingShadowsStyle fontSize={fontSize} color={color} $wrapText={wrapText}>
+      <HeadingShadowsStyle fontSize={fontSize} color={color} $wrapText={wrapText} units={units}>
         {children}
       </HeadingShadowsStyle>
-      <HeadingShadowsStyle fontSize={fontSize} color={color} $wrapText={wrapText}>
+      <HeadingShadowsStyle fontSize={fontSize} color={color} $wrapText={wrapText} units={units}>
         {children}
       </HeadingShadowsStyle>
-      <HeadingStyle fontSize={fontSize} color={color} $wrapText={wrapText}>
+      <HeadingStyle fontSize={fontSize} color={color} $wrapText={wrapText} units={units}>
         {children}
       </HeadingStyle>
-      <HeadingPatternStyle fontSize={fontSize} color={color} $wrapText={wrapText}>
+      <HeadingPatternStyle fontSize={fontSize} color={color} $wrapText={wrapText} units={units}>
         {children}
       </HeadingPatternStyle>
     </HeadingContainer>
